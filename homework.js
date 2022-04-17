678aba3ce6f9641dbe8f6b1f318a46a7e269f5d7
Jiamenglan <100423779+Jiamenglan@users.noreply.github.com>
2022-04-17 19:14:34 +0800

    Add files via upload

---
+++ homework.js
@@ -0,0 +1,105 @@
+function sayHello(){
+  var response = prompt("What's your name?");
+  alert("Hello " + response  + ",welcome to my world,let's play a game!");
+}
+sayHello();
+
+function answera(){
+  alert("Hot, you can easily catch cold");
+}
+function answerb(){
+  alert("Because seven eight(ate) nine");
+}
+function answerc(){
+  alert("Because I want to see the butterfly");
+}
+function answerd(){
+  alert("Turkey");
+}
+function answere(){
+  alert("An envelop");
+}
+function answerf(){
+  alert("All");
+}
+function answerg(){
+  alert("I C U");
+}
+
+function getanswera(){
+  var x="";
+  var response = prompt("Write your answer here");
+  if (response === "Hot"){
+    x="You're right!"
+  }
+  else{
+    x="Think a little bit more."
+  }
+  alert(x);
+}
+function getanswerb(){
+  var x="";
+  var response = prompt("Write your answer here");
+  if (response === "Because seven eight(ate) nine"){
+    x="You're right!"
+  }
+  else{
+    x="Think a little bit more."
+  }
+  alert(x);
+}
+function getanswerc(){
+  var x="";
+  var response = prompt("Write your answer here");
+  if (response === "Because I want to see the butterfly"){
+    x="You're right!"
+  }
+  else{
+    x="Think a little bit more."
+  }
+  alert(x);
+}
+function getanswerd(){
+  var x="";
+  var response = prompt("Write your answer here");
+  if (response === "Turkey"){
+    x="You're right!"
+  }
+  else{
+    x="Think a little bit more."
+  }
+  alert(x);
+}
+function getanswere(){
+  var x="";
+  var response = prompt("Write your answer here");
+  if (response === "An envelop"){
+    x="You're right!"
+  }
+  else{
+    x="Think a little bit more."
+  }
+  alert(x);
+}
+function getanswerf(){
+  var x="";
+  var response = prompt("Write your answer here");
+  if (response === "All"){
+    x="You're right!"
+  }
+  else{
+    x="Think a little bit more."
+  }
+  alert(x);
+}
+function getanswerg(){
+  var x="";
+  var response = prompt("Write your answer here");
+  if (response === "I C U"){
+    x="You're right!"
+  }
+  else{
+    x="Think a little bit more."
+  }
+  alert(x);
+}
